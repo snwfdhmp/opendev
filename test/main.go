dc53acc6d5f07a8012b83c542b8e6699015afd2a
Martin Joly <snwfdhmp@gmail.com>
2018-02-11 21:29:37 +0100

    commit

--- test/main.go
+++ test/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/src-d/go-git.v4"
 )
 
 func main() {
 	fmt.Println("PRE RECEIVE HOOK START")
+	abs, _ := filepath.Abs("./")
+	fmt.Println("wd:", abs)
 	gitRepo, err := git.PlainOpen("./")
 	if err != nil {
 		fmt.Println("fatal:", err)
