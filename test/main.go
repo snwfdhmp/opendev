d19199c436e553961afafb2d620f259408e7abe8
Martin Joly <snwfdhmp@gmail.com>
2018-02-11 21:34:04 +0100

    commit

--- test/main.go
+++ test/main.go
@@ -26,16 +26,5 @@ func main() {
 
 	fmt.Println("head:", head.String())
 
-	fmt.Println("env('GITHUB_USER_LOGIN'):", os.Getenv("GITHUB_USER_LOGIN"))
-	fmt.Println("env('GIT_DIR'):", os.Getenv("GIT_DIR"))
-	fmt.Println("env('GITHUB_USER_IP'):", os.Getenv("GITHUB_USER_IP"))
-	fmt.Println("env('GITHUB_REPO_NAME'):", os.Getenv("GITHUB_REPO_NAME"))
-	fmt.Println("env('GITHUB_PULL_REQUEST_AUTHOR_LOGIN'):", os.Getenv("GITHUB_PULL_REQUEST_AUTHOR_LOGIN"))
-	fmt.Println("env('GITHUB_REPO_PUBLIC'):", os.Getenv("GITHUB_REPO_PUBLIC"))
-	fmt.Println("env('GITHUB_PUBLIC_KEY_FINGERPRINT'):", os.Getenv("GITHUB_PUBLIC_KEY_FINGERPRINT"))
-	fmt.Println("env('GITHUB_PULL_REQUEST_HEAD'):", os.Getenv("GITHUB_PULL_REQUEST_HEAD"))
-	fmt.Println("env('GITHUB_PULL_REQUEST_BASE'):", os.Getenv("GITHUB_PULL_REQUEST_BASE"))
-	fmt.Println("env('GITHUB_VIA'):", os.Getenv("GITHUB_VIA"))
-
 	fmt.Println("PRE RECEIVE HOOK END")
 }
