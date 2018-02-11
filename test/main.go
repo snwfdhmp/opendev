6b3272a89ede5edad924fe7506d93d21c17ad8f1
Martin Joly <snwfdhmp@gmail.com>
2018-02-11 21:31:04 +0100

    commit

--- test/main.go
+++ test/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("PRE RECEIVE HOOK START")
 	abs, _ := filepath.Abs("./")
 	fmt.Println("wd:", abs)
-	gitRepo, err := git.PlainOpen("./")
+	gitRepo, err := git.PlainOpen("./..")
 	if err != nil {
 		fmt.Println("fatal:", err)
 		return
